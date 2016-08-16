5837226375679c7d608bfca88c18d05cc668b711
Paul Colomiets <paul@colomiets.name>
2016-08-16 23:49:35 +0300

    Shakespeare with redis

--- app/shakespeare.py
+++ app/shakespeare.py
@@ -3,11 +3,13 @@ import random
 from xml.sax import parse, ContentHandler
 import falcon
 import json
+import redis
 import sys
 from io import StringIO
 
 
 DIR = pathlib.Path('sonets')
+redis_conn = redis.StrictRedis()
 
 
 class Parser(ContentHandler):
@@ -22,7 +24,9 @@ class Main:
     def on_get(self, req, resp):
         """Handles GET requests"""
         filename = random.choice(list(DIR.iterdir()))
+        visitor_no = redis_conn.incr('visitors')
         buf = StringIO()
+        buf.write("<p>You're visitor #{}</p>".format(visitor_no))
         buf.write('<pre>')
         parse(str(filename), Parser(buf))
         buf.write('</pre>')
