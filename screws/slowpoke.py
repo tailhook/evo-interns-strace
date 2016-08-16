dbeed0e2cbe81e04b3350de7df4481c585a54609
Paul Colomiets <paul@colomiets.name>
2016-08-17 01:54:38 +0300

    Add nginx demo

--- screws/slowpoke.py
+++ screws/slowpoke.py
@@ -1,22 +1,45 @@
 import asyncio
 from cowpy import cow
+import aiohttp
 from aiohttp import web
+
 
 async def handle(request):
     text = str(cow.Moose().milk('Yay!')).replace('<', '(').replace('>', ')')
     return web.Response(body=text.encode('utf-8'), content_type='text/plain')
+
 
 async def handle_slow(request):
     await asyncio.sleep(10)
     text = str(cow.Moose().milk('Yay!')).replace('<', '(').replace('>', ')')
     return web.Response(body=text.encode('utf-8'))
 
-async def handle_hello(request):
-    return web.Response(body='hello'.encode('utf-8'))
+
+async def websocket_handler(request):
+
+    for k, v in request.headers.items():
+        print(k, v)
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+
+    async for msg in ws:
+        if msg.tp == aiohttp.MsgType.text:
+            if msg.data == 'close':
+                await ws.close()
+            else:
+                ws.send_str(msg.data + '/answer')
+        elif msg.tp == aiohttp.MsgType.error:
+            print('ws connection closed with exception %s' %
+                  ws.exception())
+
+    print('websocket connection closed')
+
+    return ws
+
 
 app = web.Application()
 app.router.add_route('GET', '/', handle)
 app.router.add_route('GET', '/slow_cow', handle_slow)
-app.router.add_route('GET', '/hello', handle_hello)
+app.router.add_route('GET', '/ws', websocket_handler)
 
 web.run_app(app)
